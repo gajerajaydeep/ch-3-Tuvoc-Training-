449442cce3c525960c64b73913f565d9e8813b85
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-02-04 18:24:17 +0530

    complete Task-b, fetch userData using useEffect()

--- ch-3/src/App.js
+++ ch-3/src/App.js
@@ -1,10 +1,12 @@
 
 import Toggal from "./Ch_3_Tasks/Toggal";
+import Task_b from "./Ch_3_Tasks/Task_b";
 
 function App() {
   return (
     <>
-      <Toggal/>
+      {/* <Toggal/> */}
+      <Task_b/>
     </>
   );
 }
---
+++ ch-3/src/Ch_3_Tasks/Task_b.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Task_b() {
+
+    const [apiData, setApidata] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((responce) => responce.json())
+            .then((data) => setApidata(data))
+            .catch((error) => console.log(error)
+            )
+
+    }, [])
+    return (
+        <>
+            <h2>Fetch User Data using JSONPlaceholder API</h2>
+            <ul>
+                {apiData.map((value, index) => (
+                    <li key={index}>
+                         UserId = {value.id} <br />
+                        Name : {value.name} <br />
+                        email : {value.email} <br />
+                        Address : {value.address.street}
+                    </li>
+                ))}
+            </ul>
+        </>
+
+
+    )
+}
