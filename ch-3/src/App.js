472c3e7882599bcb377cfdb07d214b56383e7696
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-02-04 14:03:11 +0530

    (light/dark theme) Task-a complete

---
+++ ch-3/src/App.js
@@ -0,0 +1,12 @@
+
+import Toggal from "./Ch_3_Tasks/Toggal";
+
+function App() {
+  return (
+    <>
+      <Toggal/>
+    </>
+  );
+}
+
+export default App;
---
+++ ch-3/src/Ch_3_Tasks/Toggal.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './Toggal.css';
+
+export default function Toggal() {
+
+
+
+    const [isBtn, setIsBtn] = useState(false)
+
+    const handleToggalBtn = () => {
+        setIsBtn(!isBtn)
+       
+    }
+
+
+
+return (
+    <>
+       <div className="toggal-switch"  style={{backgroundColor : isBtn ? 'white' : 'black'}}>
+        <div className={`switch ${isBtn ? 'on' : 'off' }`}>
+            <button onClick={handleToggalBtn} className='toggal-btn'>{isBtn ? 'on' : 'off' }</button>
+        </div>
+       </div>
+    </>
+)
+
+}
