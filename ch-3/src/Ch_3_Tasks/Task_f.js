b79c08fc7737e7ff9356a9996beb79dabc345884
Jaydeep Gajera <jaydeepgajera63526@gmail.com>
2025-02-24 10:36:13 +0530

    changes on Task_f is completed

--- ch-3/src/Ch_3_Tasks/Task_f.js
+++ ch-3/src/Ch_3_Tasks/Task_f.js
@@ -1,42 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo, useCallback } from 'react';
 
 export default function Task_f() {
+  const [number, setNumber] = useState([1, 2, 3, 4, 5]);
+  const [sum, setSum] = useState(0);
 
-    const [number, setNumber] = useState([1, 2, 3, 4, 5])
-    const [sum, setSum] = useState(0)
+  
+  const sumNumber = useMemo(() => {
+    const squaredNumbers = number.map((v) => v * v);
+    return squaredNumbers.reduce((accumulator, current) => accumulator + current, 0);
+  }, [number]); 
 
-    let squreNumber = number.map((v) => (v * v));
-    console.log(squreNumber);
-
-    let sumNumber = squreNumber.reduce((acumulatore, current) => {
-        return acumulatore + current;
-    })
  
+  const getSum = useCallback(() => {
+    setSum(sumNumber); 
+  }, [sumNumber]); 
 
-    const getSum = () => (
-        setSum(sum + sumNumber)
-     
-        
-    )
-    const handleInputChange = (e) =>(
-       setNumber([...number,e.target.value]) 
-    )
-    return (
-        <>
-        <label htmlFor="">Enter Value to Add in List : &nbsp; </label>
-                <input type="text" onChange={handleInputChange}/>
 
-            <ul>
-                {
-                    number.map((v, i) => (
-                        <li key={i}>{v}</li>
-                    ))
-                }
-            </ul>
-            <h1>Squred Sum Of All Values is : {sum}</h1>
-            <button onClick={getSum}>Sum</button>
-        </>
-    )
+  const handleInputChange = useCallback((e) => {
+    const inputValue = parseInt(e.target.value, 10);
+
+    if (!isNaN(inputValue)) {
+      setNumber((prevNumbers) => [...prevNumbers, inputValue]);
+    }
+  }, []); 
+
+  return (
+    <>
+      <label htmlFor="">Enter Value to Add in List : &nbsp; </label>
+      <input type="text" onChange={handleInputChange} />
+
+      <ul>
+        {number.map((v, i) => (
+          <li key={i}>{v}</li>
+        ))}
+      </ul>
+      <h1>Squared Sum Of All Values is : {sum}</h1>
+      <button onClick={getSum}>Sum</button>
+    </>
+  );
 }
-
-
